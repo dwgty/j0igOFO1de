8df3abc4ef7555889626e6521593877b80b0c9ed
dwgty <khoirullutfianwar@gmail.com>
2025-07-25 19:59:55 +0700

    upload deta5.js

---
+++ deta5.js
@@ -0,0 +1,32 @@
+window.videos = [{
+  "judul": "Overflow",
+  "rating": 6.6,
+  "jumlah": 8,
+  "status": "Completed",
+  "season": 1,
+  "tipe": "OVA",
+  "studio": "Studio Hokiboshi",
+  "sinopsis": "Kazushi Sudou adalah seorang mahasiswa yang dikunjungi oleh dua sahabat masa kecilnya, Ayane dan Kotone Shirakawa. Ketika Ayane mengetahui bahwa Kazushi tidak hanya lupa membeli pudingnya tetapi juga menggunakan losion khusus miliknya saat berendam, ia memutuskan untuk membalas dendam dan bergabung dengan Kazushi di bak mandinya bersama Kotone. Akankah Kazushi yang mesum mampu tetap acuh tak acuh terhadap mereka berdua?",
+  "aired": "2025-07-11",
+  "genre": [
+    "Romance",
+    "Ecchi",
+    "Harem"
+  ],
+  "hastag": [
+    "Romance",
+    "Ecchi",
+    "Harem"
+  ],
+  "thumb": "https://i.postimg.cc/gjdxqqDq/images.jpg",
+  "link": [
+    "https://qu.ax/CmouR.mp4",
+    "https://qu.ax/svSUo.mp4",
+    "https://qu.ax/irnUh.mp4",
+    "https://qu.ax/Rtowg.mp4",
+    "https://qu.ax/UHsQg.mp4",
+    "https://qu.ax/TMUVR.mp4",
+    "https://qu.ax/vilBn.mp4",
+    "https://qu.ax/HqLpP.mp4"
+  ]
+}];
