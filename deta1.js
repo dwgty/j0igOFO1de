f4d7c342e82472aa479fe3799c12a90e38ffe7fc
dwgty <khoirullutfianwar@gmail.com>
2025-07-28 23:49:00 +0700

    Update deta1.js

--- deta1.js
+++ deta1.js
@@ -1,25 +1,2 @@
-
-
-
-// Tambahan dari form
-{
-  "title": "Boku no Risou no Isekai Seikatsu",
-  "mini-info-rtg": "6.6",
-  "mini-info-eps": "1",
-  "status": "Completed",
-  "tipe": "OVA",
-  "studio": "Studio 1st",
-  "sinopsis": "Di dunia yang ditujunya, ia menjadi bocah kemonomimi. Seorang pria lajang berusia 40 tahun tanpa nama melakukan masturbasi terlalu banyak hingga kehilangan kesadaran. Ia mengira bahwa ia telah menyelesaikan hidupnya di bumi. Namun, saat ia sadar kembali, ia mendapati seorang gadis manis dengan raut wajah khawatir di hadapannya.\\nNamanya Misia, dan dia memanggil pria itu Norn.",
-  "aired-start": "2025-07-10",
-  "thumbnail": "https://i.postimg.cc/3NQ66DxJ/images.jpg",
-  "tanda-populer": "T",
-  "direktur": "HiyaNime",
-  "season": "Musim 1",
-  "genre": "Fantasy, Isekai, Harem",
-  "hastag": [
-    "Fantasy",
-    "Isekai",
-    "Harem"
-  ],
-  "eps1": "https://qu.ax/gwNvp.mp4"
-}
+window.videos = [
+];
