edcb15a22f6ea22b60230708fbfa2d603098a5b0
dwgty <khoirullutfianwar@gmail.com>
2025-07-29 22:13:27 +0700

    Update deta1.js

--- deta1.js
+++ deta1.js
@@ -1,1 +1,26 @@
-
+window.videos = [
+{
+  "title": "Overflow",
+  "mini-info-rtg": "6.6",
+  "mini-info-eps": "8",
+  "status": "Completed",
+  "tipe": "OVA",
+  "studio": "Studio Hokiboshi",
+  "sinopsis": "Kazushi Sudou adalah seorang mahasiswa yang dikunjungi oleh dua sahabat masa kecilnya, Ayane dan Kotone Shirakawa. Ketika Ayane mengetahui bahwa Kazushi tidak hanya lupa membeli pudingnya tetapi juga menggunakan losion khusus miliknya saat berendam, ia memutuskan untuk membalas dendam dan bergabung dengan Kazushi di bak mandinya bersama Kotone. Akankah Kazushi yang mesum mampu tetap acuh tak acuh terhadap mereka berdua?",
+  "aired-start": "2025-07-11",
+  "thumbnail": "https://i.postimg.cc/gjdxqqDq/images.jpg",
+  "tanda-populer": "T",
+  "direktur": "HiyaNime",
+  "season": "Musim 1",
+  "genre": "Romance, Ecchi, Harem",
+  "hastag": [ "Romance", "Ecchi", "Harem", "keluarga", "adik", "kakak" ],
+  "eps1": "https://qu.ax/CmouR.mp4",
+  "eps2": "https://qu.ax/svSUo.mp4",
+  "eps3": "https://qu.ax/irnUh.mp4",
+  "eps4": "https://qu.ax/Rtowg.mp4",
+  "eps5": "https://qu.ax/UHsQg.mp4",
+  "eps6": "https://qu.ax/TMUVR.mp4",
+  "eps7": "https://qu.ax/vilBn.mp4",
+  "eps8": "https://qu.ax/HqLpP.mp4"
+}
+];
