24f0ac9af262d19050f1e8d2405a96a4c481703e
dwgty <khoirullutfianwar@gmail.com>
2025-09-03 19:37:56 +0700

    Tambah data ke fq4r5tt.js

--- xc3srd/fq4r5tt.js
+++ xc3srd/fq4r5tt.js
@@ -1,2 +1,34 @@
-window.videos = [
+window.videos = [,
+  {
+  "title": "The New Gate",
+  "mini-info-rtg": "6.4",
+  "mini-info-eps": "12",
+  "status": "Complete",
+  "tipe": "Series",
+  "studio": "Yokohama Animation Lab, Cloud Hearts",
+  "sinopsis": "The New Gate adalah gim daring yang tiba-tiba menjebak pemainnya tetap berada di dalam dunia gim yang menyiksa.\n\nShin, salah satu pemain terkuat dalam gim ini. Setelah perjuangan mati-matian melawan bos terakhir Origin, akhirnya pedangnya Shin menembus tubuh Origin. Gim berhasil diselesaikan dan para pemain yang terjebak dapat keluar.\n\nSaat para pemain logout dan Shin hendak berpisah dengan dunia gim ini, sebuah gerbang baru tiba-tiba terbuka...\n\nDengan diselimuti cahaya, apa yang terbentang luas di hadapan Shin adalah dunia 500 tahun setelah gim The New Gate!.",
+  "aired-start": "2025-09-03",
+  "thumbnail": "https://i.postimg.cc/137LTC5s/images.jpg",
+  "tanda-populer": "F",
+  "direktur": "Admin",
+  "season": "Musim 1",
+  "genre": "Action, Adventure, Fantasy",
+  "hastag": [
+    "Action",
+    "Adventure",
+    "Fantasy"
+  ],
+  "eps1": "https://storages.animein.net/The%20New%20Gate%2F1-480p-1736381671570.mp4",
+  "eps2": "https://storages.animein.net/The%20New%20Gate%2F2-480p-1735267737251.mp4",
+  "eps3": "https://storages.animein.net/The%20New%20Gate%2F3-360p-1736657419200.mp4",
+  "eps4": "https://storages.animein.net/The%20New%20Gate%2F4-360p-1736657419204.mp4",
+  "eps5": "https://storages.animein.net/The%20New%20Gate%2F5-360p-1736657419212.mp4",
+  "eps6": "https://storages.animein.net/The%20New%20Gate%2F6-360p-1736657419221.mp4",
+  "eps7": "https://storages.animein.net/The%20New%20Gate%2F7-360p-1736657419227.mp4",
+  "eps8": "https://storages.animein.net/The%20New%20Gate%2F8-360p-1736657419231.mp4",
+  "eps9": "https://storages.animein.net/The%20New%20Gate%2F9-360p-1736657419237.mp4",
+  "eps10": "https://storages.animein.net/The%20New%20Gate%2F10-360p-1736657419244.mp4",
+  "eps11": "https://storages.animein.net/The%20New%20Gate%2F11-360p-1736657419248.mp4",
+  "eps12": "https://storages.animein.net/The%20New%20Gate%2F12-360p-1736657419252.mp4"
+}
 ];
